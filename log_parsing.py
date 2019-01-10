8491298208ea7a04468f36d6ef15af222500050b
Neeba Chandy <nechandy@cisco.com>
2019-01-10 08:32:28 -0800

    Added comments

--- log_parsing.py
+++ log_parsing.py
@@ -4,6 +4,20 @@ import os
 import datetime
 
 class Job():
+
+    """
+    A class definition to hold all the information about a job.
+
+        name   : string
+            Name of the job.
+        start_time : string
+               When the job started
+              end_time : string
+               When the job finished
+        status: number
+            Status of the job. If it is 0 means success, all othe values means failure
+
+    """
     def __init__(self, name="", start_time=0, end_time=0, status=0):
         self.name = name
         self.start_time = start_time
@@ -16,12 +30,26 @@ class Job():
         return datetime.datetime.strptime(self.end_time, '%Y-%m-%d.%H:%M:%S')
 
 def read_file(file_name):
+
+    """
+    Take input name of the file and return the content.
+    param file_name: File name which has to be read
+    return content: Content of log file as a list of string
+    """
+
     with open (file_name) as fd:
         content = fd.readlines()
     content = [x.strip() for x in content]
     return content
 
 def parse_file_content(content_of_log_file):
+
+    """
+    Parse the content of the log file and create a dictinary.
+    param content_of_log_file: Content of log file as a list of string
+    return jobs_dict: a dictionary with build name as key , build details as values
+    """
+    
     search_regex = re.compile('^=== \[(?P<process_time>.+)\] :(?P<process_state>.+): (?P<process_name>[^\s]+)\s*(?P<process_status>[0-9])*$')
     jobs_dict = {}
     for log_line in content_of_log_file:
@@ -45,6 +73,13 @@ def parse_file_content(content_of_log_file):
     return jobs_dict
 
 def create_html_file(jobs_dict):
+
+    """
+    Creates an output html file with graphical and tabular view.
+    param jobs_dict: a dictionary with build name as key , build details as values
+    return: None
+    """
+
     template_file_name = ("log_template.html")
     with open(template_file_name) as html_src:
         html_template = html_src.read()
